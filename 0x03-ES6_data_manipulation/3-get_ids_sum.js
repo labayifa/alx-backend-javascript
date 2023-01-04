1970f37b590e38152ecaf2f3deb1eb49811129e9
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-04 02:27:14 +0100

    Data manip

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,3 @@
 export default function getStudentIdsSum(students) {
-  if (!(students instanceof Array)) {
-    return 0;
-  }
   return students.reduce(((previousValue, currentValue) => previousValue += currentValue.id), +0);
 }
