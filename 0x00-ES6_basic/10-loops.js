6ce665326b9ff869e799981956bd643078be8ecf
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-02-04 12:53:07 +0100

    ok

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const appendedArray = [];
-  for (let idx of array) {
-    const value = appendString + idx;
-    appendedArray.push(value);
+  for (let item of array) {
+    item = appendString + item;
+    appendedArray.push(item);
   }
 
   return appendedArray;
